6e4530d625cf0e800062827eaced7d28aaec1c1b
Ismael Jônatas <77457233+Ismael-Jonatas@users.noreply.github.com>
2025-09-10 14:54:34 -0300

    adicionando backend

---
+++ backend/desafio-pleno/src/main/java/com/prodabel/desafiopleno/DesafioPlenoApplication.java
@@ -0,0 +1,13 @@
+package com.prodabel.desafiopleno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPlenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPlenoApplication.class, args);
+	}
+
+}
